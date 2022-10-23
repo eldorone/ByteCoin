7dcdbeff7f0d6673ce9416d71ac6fe10869c093f
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-24 01:23:57 +0900

    developing the application

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 protocol CoinManagerDelegate {
-    func didUpdatePrice(_ weatherManger: WeatherManager, weather: WeatherModel)
+    func didUpdatePrice(price: String, currency: String)
     func didFailWithError(error: Error)
 }
 
 struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "C7029074-B375-45E8-8C25-2F81FDE25866"
@@ -20,38 +22,39 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD",
                          "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD",
                          "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"]
-   
-    var delegate: CoinManagerDelegate?
+    
     
     func getCoinPrice(for currency: String) {
+        
         let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
-        performRequest(with: urlString)
-    }
-    
-    func performRequest(with urlString: String) {
+        print(urlString)
         
         if let url = URL(string: urlString) {
+            
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
-                    print(error!)
+                    self.delegate?.didFailWithError(error: error!)
                     return
                 }
                 
-                let dataString = String(data: data!, encoding: .utf8)
-                print(dataString!)
+                if let safeData = data {
+                    if let bitCoinPrice = self.parseJSON(safeData) {
+                        let priceString = String(format: "%.2f", bitCoinPrice)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
             }
             task.resume()
         }
     }
     
-    func parseJSON(_ coinData: Data) -> String? {
+    func parseJSON(_ data: Data) -> Double? {
+        
         let decoder = JSONDecoder()
         do {
-            let decodedData = try decoder.decode(CoinData.self, from: coinData)
-            print(decodedData.rate)
-            let lastPrice = String(format: "%.2f", decodedData.rate)
-    
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
             print(lastPrice)
             return lastPrice
             
@@ -60,8 +63,4 @@ struct CoinManager {
             return nil
         }
     }
-    
-    
-    
-    
 }
