36383eeb7ce3fd093456031eb92a7ce8dac365aa
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-24 00:01:49 +0900

    Update CoinManager.swift

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -10,11 +10,30 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "C7029074-B375-45E8-8C25-2F81FDE25866"
     
     func getCoinPrice(for currency: String) {
-        
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(with: urlString)
     }
+    
+    func performRequest(with urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error)
+                    return
+                }
+                
+                let dataString = String(data: data!, encoding: .utf8)
+                print(dataString)
+            }
+            task.resume()
+        }
+    }
+    
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
