593b17e611eeb8aef534c100da66277693ccfa8b
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-23 22:49:55 +0900

    developing the application

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -7,24 +7,22 @@
 
 import UIKit
 
-let coinManager = CoinManager(currenyArray: currenyArray)
-
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
+    
+    let coinManager = CoinManager()
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
     
-    
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -33,6 +31,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return coinManager.currenyArray[row]
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
     }
 }
