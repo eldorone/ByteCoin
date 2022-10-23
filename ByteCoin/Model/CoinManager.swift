a1f261b7b281d2dfca5a2e4cf96953ce4c95f72b
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-24 00:26:16 +0900

    developing the application

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,10 +7,21 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    //func didUpdateWeather(_ weatherManger: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "C7029074-B375-45E8-8C25-2F81FDE25866"
+    
+    let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD",
+                         "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD",
+                         "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"]
+   
+    var delegate: CoinManagerDelegate?
     
     func getCoinPrice(for currency: String) {
         let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
@@ -23,19 +34,31 @@ struct CoinManager {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
-                    print(error)
+                    print(error!)
                     return
                 }
                 
                 let dataString = String(data: data!, encoding: .utf8)
-                print(dataString)
+                print(dataString1)
             }
             task.resume()
         }
     }
     
+    func parseJSON(_ coinData: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: coinData)
+            let lastPrice = decodedData.rate
+            return lastPrice
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
     
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
+    
     
 }
